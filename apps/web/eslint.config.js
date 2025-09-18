a053a88b2871fbc367584206d82a73ae7cc8227e
Lucien Shaw <myxlc55@outlook.com>
2025-09-18 20:10:06 +0800

    chore: 添加typescript相关的eslint配置

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -11,15 +11,48 @@ const compat = new FlatCompat({
 module.exports = defineConfig([
   globalIgnores(['**/dist/', '**/node_modules/', '**/build/']),
   {
-    extends: compat.extends('prettier', 'plugin:react/recommended'),
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+      globals: {},
+    },
+    extends: compat.extends(
+      'prettier',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+    ),
     settings: {
       react: {
         version: 'detect',
+        jsxRuntime: 'automatic',
       },
     },
     rules: {
       'react/jsx-key': 'off',
+      'react/react-in-jsx-scope': 'off',
+      // 所有引用了@react-three/fiber的文件，在文件中单独禁用这个规则
+      // 方法是使用文件头标记 /* eslint-disable react/no-unknown-property */
+      // 'react/no-unknown-property': 'off',
     },
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    extends: compat.extends('prettier', 'plugin:react/recommended'),
+    settings: {
+      react: {
+        version: 'detect',
+        jsxRuntime: 'automatic',
+      },
+    },
+    rules: {
+      'react/jsx-key': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
   },
 ])
